e0f8ba14d2f40d34de16b9ddd7f6e465ec389a1b
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2018-11-19 14:07:06 +0700

    fix v_login

--- application/views/V_Login.php
+++ application/views/V_Login.php
@@ -27,16 +27,16 @@
 						<div class="panel-heading" style="padding-bottom: 0px; border-bottom: 1px solid #e0e0e0">
 							<div class="row text-center">
 								<div class="col-sm-6 col-md-6">
-									<a href="<?php echo base_url() ?>login" id="login-form-link" style="text-decoration: none; color: #9e9e9e;">Sign In</a>
+									<b><a href="<?php echo base_url() ?>" id="login-form-link" style="text-decoration: none; color: #2979ff;">Sign In</a></b>
 								</div>
 								<div class="col-sm-6 col-md-6">
-									<b><a href="<?php echo base_url() ?>login/register" id="register-form-link" style="text-decoration: none; color: #2979ff;">Register</a></b>
+									<a href="<?php echo base_url() ?>login/register" id="register-form-link" style="text-decoration: none; color: #9e9e9e">Register</a>
 								</div>
 							</div>
 							<hr>
 						</div>
 						<div class="panel-body" style="padding-top: 0px;">
-							<div class="row" id="register-form">
+							<div class="row" id="login-form">
 								<div class="col-sm-12 col-md-12">
 									<div class="row">
 										<div class="col-sm-12 col-md-12">
@@ -53,28 +53,20 @@
 									</div><hr>
 									<div class="row" style="margin-top: 40px;">
 										<div class="col-sm-12 col-md-12">
+											
 												<div class="form-group">
-													<label class="control-label" for="name">Name</label>
-													<input class="form-control" type="text" name="name" value="" id="name" placeholder="Name">
-												</div>
-												<div class="form-group" style="margin-top: 20px;">
 													<label class="control-label" for="username">Username</label>
 													<input class="form-control" type="text" name="username" value="" id="username" placeholder="Username">
-												</div>
-												<div class="form-group" style="margin-top: 20px;">
-													<label class="control-label" for="email">Email</label>
-													<input class="form-control" type="email" name="email" value="" id="email" placeholder="Email">
 												</div>
 												<div class="form-group" style="margin-top: 20px;">
 													<label class="control-label" for="password">Password</label>
 													<input class="form-control" type="password" name="password" value="" id="password" placeholder="Password">
 												</div>
+												<p style="float: right; margin: 10px 0 20px;"><a href="#">Forget Password?</a></p>
 												<div class="form-group">
-													<p style="font-size: 13px; color: #c4c4c4;"><input type="checkbox" name="accept" value="accept"> Dengan ini anda setuju dengan <b>kebijakan privasi</b> dan <b>peraturan</b> yang ada</p>
+													<input type="button" name="login-submit" id="login-submit" class="btn btn-default col-md-12" style="padding: 10px 0; margin-bottom: 20px; background-color: #2979ff; color: #fff" onclick="cek_login();" value="Sign In">
 												</div>
-												<div class="form-group" style="margin-top: 24px;">
-													<input type="button" name="register-submit" class="btn btn-info col-md-12" id="register-submit" style="padding: 10px 0; margin-bottom: 20px; background-color: #2979ff; color: #fff" value="Create Account" onclick="register();">
-												</div>
+											
 										</div>
 									</div>
 								</div>
@@ -87,45 +79,37 @@
 	</div>
 </body>
 <script type="text/javascript">
-	function register(){
+	function cek_login()
+	{
 	//Mengambil value dari input username & Password
-	var name = $('#name').val();
 	var username = $('#username').val();
-	var email = $('#email').val();
 	var password = $('#password').val();
 	//Ubah alamat url berikut, sesuaikan dengan alamat script pada komputer anda
-	var url_register = '<?php echo base_url()?>login/aksi_register/';
-	var url_home	 = '<?php echo base_url()?>login/';
+	var url_login	 = '<?php echo base_url()?>Login/cek_login/';
+	var url_home	 = '<?php echo base_url()?>';
 	
 	//Ubah tulisan pada button saat click login
-	$('#register-submit').attr('value','Please wait ...');
+	$('#login-submit').attr('value','Please wait ...');
+	
 	//Gunakan jquery AJAX
 	$.ajax({
-		url		: url_register,
+		url		: url_login,
 		//mengirimkan username dan password ke script login.php
-		data	: 	'username='+username+
-					'&password='+password+
-					'&email='+email+
-					'&nama='+name, 
+		data	: 'username='+username+'&password='+password, 
 		//Method pengiriman
 		type	: 'POST',
 		//Data yang akan diambil dari script pemroses
 		dataType: 'html',
 		//Respon jika data berhasil dikirim
 		success	: function(pesan){
-			if(pesan=='1'){
-				//Arahkan ke halaman home
-				alert('Email sudah digunakan');
-				$('#register-submit').attr('value','Try again ...');
-			}else if(pesan == '2'){
-				//Cetak peringatan untuk username & password salah
-				alert('Username sudah digunakan');
-				$('#register-submit').attr('value','Try again ...');
-			}else if(pesan == '3'){
-				alert('Username dan email sudah digunakan');
-				$('#register-submit').attr('value','Try again ...');
-			}else{
+			if(pesan=='TRUE'){
+				//Arahkan ke halaman admin jika script pemroses mencetak kata ok
 				window.location = url_home;
+			}
+			else{
+				//Cetak peringatan untuk username & password salah
+				alert(pesan);
+				$('#login-submit').attr('value','Try again ...');
 			}
 		},
 	});
