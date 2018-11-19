b9990aae7967136fe18912236a98af7e0f69dc08
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2018-11-19 14:19:09 +0700

    add M_Login

--- application/models/M_Login.php
+++ application/models/M_Login.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_Login extends CI_Model {
 
+	function get_where($where){
+		$this->db->where($where);
+		return $this->db->get('users');
+	}
+
+	function register($data){
+		$this->db->insert('users',$data);
+	}
 	
 
 }
